bf8d161e590691840eeccea25ab17b6e73eceb1d
Keith Morrison <keithm@infused.org>
2010-12-30 17:56:59 -0800

    Use a normal attr_accessor

--- lib/ged2csv/transformation.rb
+++ lib/ged2csv/transformation.rb
@@ -3,10 +3,11 @@
 module Ged2Csv
   module Transformation
     class DelimitedFile
-      cattr_accessor :record_delimiter
-      @@record_delimiter = ','
+      attr_accessor :record_delimiter
       
       def initialize
+        @record_delimiter = ','
+        
         yield self if block_given?
       end
       
